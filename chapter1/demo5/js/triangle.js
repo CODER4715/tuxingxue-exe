5cdb25161ebf302efa2b68e5906801ad85647851
CODER4715 <70872936+CODER4715@users.noreply.github.com>
2022-09-19 15:07:43 +0800

    Add files via upload

---
+++ chapter1/demo5/js/triangle.js
@@ -0,0 +1,64 @@
+"use strict";
+
+var gl;
+var points;
+
+window.onload = function init(){
+	var canvas = document.getElementById( "triangle-canvas" );
+	gl = WebGLUtils.setupWebGL( canvas );
+	if( !gl ){
+		alert( "WebGL isn't available" );
+	}
+
+	// Three Vertices
+	var vertices = [
+		-1.0, -1.0, 
+		 0.0,  1.0, 
+		 1.0, -1.0,
+	];
+	
+	var colors = [
+		1.0,0.5,0.7,1.0,
+		0.5,0.7,0.7,1.0,
+		0.9,0.9,0.2,1.0,
+	];
+
+	// Configure WebGL
+	gl.viewport( 0, 0, canvas.width, canvas.height );
+	gl.clearColor( 1.0, 1.0, 1.0, 0.0 );
+
+	// Load shaders and initialize attribute buffers
+	var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+	gl.useProgram( program );
+
+	// Load the data into the GPU
+	var bufferId = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( vertices ), gl.STATIC_DRAW );
+
+    
+    
+		
+
+	// Associate external shader variables with data buffer
+	var vPosition = gl.getAttribLocation( program, "vPosition" );
+	var a_Color = gl.getAttribLocation( program, 'a_Color');
+	gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+	var colorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.DYNAMIC_DRAW);
+	gl.vertexAttribPointer(a_Color, 4, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray( vPosition );
+	gl.enableVertexAttribArray( a_Color );
+
+    
+	render();
+}
+
+function render(){
+	
+	gl.clear( gl.COLOR_BUFFER_BIT );
+	//gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+	gl.drawArrays( gl.TRIANGLES, 0, 3 );
+	//gl.drawArrays( gl.TRIANGLE_FANS, 3, 6 );
+}
